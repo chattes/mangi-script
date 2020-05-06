98dbf07b744fc728da89da907487fc33ada37a2a
Sourav Chatterjee <souravchatterjee81@gmail.com>
2020-05-07 05:03:23 +0530

    My mangi Script

--- my_ubuntu_setup.sh
+++ my_ubuntu_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Sojharo's Development Machine Setup on Ubuntu
-# Author : Sojharo
+# Sourav's Development Machine Setup on Ubuntu
+# Author : Sourav Chatterjee
 #
 # set software sources to server for united states
 #
@@ -19,6 +19,26 @@ sudo apt-get update
 # Installing build essentials
 sudo apt-get install -y build-essential libssl-dev
 
+# Install zsh
+
+sudo apt-get install -y zsh 
+
+# Vim, Curl, Python - Some random useful stuff
+sudo apt-get install -y vim nvim curl wget git python-software-properties
+sudo apt-get install -y python-dev, python-pip
+sudo apt-get install -y python3-pip
+sudo apt-get install -y libkrb5-dev
+
+# Setup oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Setup plugins for oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Setup https://github.com/wting/autojump
+
+sudo apt-get install -y autojump
+
 # Nodejs and NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 source ~/.profile
@@ -27,47 +47,13 @@ sudo nvm use 7.10.0
 node -v
 
 #nodemon
-sudo npm install -g nodemon
-sudo npm install -g loopback-cli
+sudo npm install -g nodemon trash-cli empty-trash-cli eslint pm2 forever
 
-# Mongodb, Installing and starting server
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo service mongod start
-sudo service mongod status
-
-# Forever to run nodejs scripts forever
-sudo npm install forever -g
 
 # Git - a version control system
 sudo apt-get update
-sudo apt-get install -y git xclip
+sudo apt-get install -y git xclip xsel
 
-# Grunt - an automated task runner
-sudo npm install -g grunt-cli
-
-# Bower - a dependency manager
-sudo npm install -g bower
-
-# Yeoman - for generators
-sudo npm install -g yo 
-
-
-# maven 
-sudo apt-get install maven -y
-
-# Gulp - an automated task runner
-sudo npm install -g gulp-cli
-
-# Angular FullStack - My favorite MEAN boilerplate (MEAN = MongoDB, Express, Angularjs, Nodejs)
-sudo npm install -g generator-angular-fullstack
-
-# Vim, Curl, Python - Some random useful stuff
-sudo apt-get install -y vim curl python-software-properties
-sudo apt-get install -y python-dev, python-pip
-sudo apt-get install -y libkrb5-dev
 
 # Installing JDK and JRE
 sudo apt-get install -y default-jre
@@ -90,9 +76,6 @@ sudo apt-get install -y tlp tlp-rdw smartmontools ethtool
 sudo tlp start
 sudo tlp stat
 
-# Erlang - Actor Concurrency Model based Programming Language
-sudo apt-get install -y erlang
-
 # Sticky Notes
 sudo apt-get install -y xpad
 
@@ -106,9 +89,6 @@ sudo apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 sudo apt-get install -y rpm
 sudo apt-get install -y alien dpkg-dev debhelper
 
-# Calibre - Ebook reader and converter
-sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
-
 # Dictionary Client and Server with Thesaurus
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 sudo apt-get install -y dict
@@ -121,17 +101,5 @@ sudo apt-get install -y dict-moby-thesaurus
 # For Android Studio (Gradle Daemon)
 #touch ~/.gradle/gradle.properties && echo "org.gradle.daemon=true" >> ~/.gradle/gradle.properties
 
-# Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Docker
-curl -fsSL get.docker.com -o get-docker.sh
-sh get-docker.sh
-# Docker-compose
-sudo pip install docker-compose
-# Atom 
-curl -L https://atom.io/download/deb -o atom-amd64.deb
-sudo dpkg -i atom-amd64.deb
-sudo apt-get -f install
-# Nuclide
-# apm install nuclide
+
