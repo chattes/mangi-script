dadc83bbefa3584092b42bd7732b745eb08a8ca9
sourav <souravchatterjee81@gmail.com>
2020-05-11 02:11:54 +0530

    Chnaged Docker source

--- my_ubuntu_setup.sh
+++ my_ubuntu_setup.sh
@@ -163,6 +163,19 @@ make
 sudo make install
 cd
 
+# Install Brave Browser
+
+sudo apt install apt-transport-https curl
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install -y brave-browser
+
+
 
 # All Done -- Reboot
 
